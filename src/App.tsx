3ff03e3a44ed767647e6dce51c88b8fb2559b8bf
Bruna Alencar <bruna.santosalencar@gmail.com>
2023-05-15 17:01:39 -0300

    Navbar e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import Navbar from "./componentes/estaticos/navbar/Navbar";
+import Footer from './componentes/estaticos/footer/Footer';
+import { Grid } from "@material-ui/core";
 import Home from "./paginas/home/Home";
 import './App.css'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='titulo'>Inicio</h1><h1 className='titulo'>Entre em Contato</h1>
+      <Navbar />
+        <Home />
+      <Footer />
     </>
   );
 }
